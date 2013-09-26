24878d27dfa6e3d84f8c97922bffecc69583a2a3
Anton Stoychev <antitoxic@gmail.com>
2013-09-26 20:06:24 +0300

    Added a sample test for resizing infowindow with animation

---
+++ static/js/infoboxAnimated.js
@@ -0,0 +1,37 @@
+var toggleFixedHeight = function($el, isFixed) {
+        if (isFixed) {
+            $el.height($el.height()+'px');
+            $el.css('overflow','hidden');
+        } else {
+            $el.css('height','');
+            $el.css('overflow','auto');
+        }
+}
+
+var InfoBoxAnimated = function() {
+    InfoBox.apply(this, arguments);
+}
+InfoBoxAnimated.prototype = new InfoBox();
+InfoBoxAnimated.prototype.switchContent = function($container, $hide, $show, speed) {
+    var self = this;
+    var $infoWindowContentContainer = $(this.div_);
+    toggleFixedHeight($container, true)
+    $container.animateContentSwitch($hide, $show, speed, {
+        beforeShow: function (show) {
+            show();
+            // do stuff when both slides are hidden
+        },
+        step: function(width, meta) {
+            if (meta.prop!='width') return;
+            self.pixelOffset_ = new google.maps.Size(-width/2, 0);
+            $infoWindowContentContainer.css('width', width)
+            self.draw();
+        },
+        final: function () {
+            toggleFixedHeight($container, false)
+            // scroll window if needed
+    //      var $resizeContainer = (window.opera) ? (document.compatMode=="CSS1Compat"? $('html') : $('body')) : $('html,body')
+    //      $resizeContainer.animate({scrollTop: 300},300);
+        }
+    });
+}
---
+++ static/js/jquery.animateContentSwitch.js
@@ -0,0 +1,121 @@
+// Animates the dimensional changes resulting from altering element contents
+(function ( $ )
+{
+	$.fn.animateContentSwitch = function (toHide, $toShow, speed, clbcks)
+	{
+		return this.each( function ()
+		{
+			var $this = $( this ),
+			unfinishedPreviousAnimation = $this.data('animateContentSwich.previous'),
+			originalHeight = null,
+			originalWidth = null,
+			callbacks = $.extend({
+                beforeShow: undefined,
+                step: undefined,
+                final: undefined
+            }, clbcks),
+			targetHeight = null,
+			targetWidth = null;
+
+			var $toHide = $.type(toHide)=='string' ? $this.find(toHide) : toHide;
+			if ($toHide.length>1) {
+				throw "jQuery.animateContentSwitch accepts only single elements";
+			}
+			//stop any currently running animations
+			$this.dequeue().stop();
+			originalHeight = $this.height();
+			originalWidth = $this.width();
+			$toHide.dequeue().stop();
+			$toShow.dequeue().stop();
+
+			// check if there was a previous animation by animateContent
+			// if the current toHide element is the same as the visible element
+			// from the previous animation then continue, otherwise
+			// jump to the end of the previous animation
+			if ($.type(unfinishedPreviousAnimation) != 'undefined') {
+				var prevToHide = unfinishedPreviousAnimation.start.el;
+				if ($.type(prevToHide)=='string') {
+					prevToHide = $this.find(prevToHide);
+				}
+				var visibleStep = prevToHide.is(':visible') ? unfinishedPreviousAnimation.start : unfinishedPreviousAnimation.target;
+				var prevVisibleEl = $.type(visibleStep.el)=='string' ? $this.find(visibleStep.el) : visibleStep.el;
+				prevVisibleEl.dequeue().stop();
+				// check if the visible element from previous animation is not the element that
+				// has to be hidden from this current animation
+				if (
+					!(
+						($.type(toHide)=='string' && prevVisibleEl.is(toHide)) ||
+						($.type(toHide)!='string' && prevVisibleEl.get(0) == toHide.get(0))
+					)
+				) {
+					$this.css('height',unfinishedPreviousAnimation.target.height);
+					unfinishedPreviousAnimation.start.el.dequeue().stop().hide();
+					unfinishedPreviousAnimation.target.el.dequeue().stop().show();
+				} else {
+					originalHeight = visibleStep.height;
+					originalWidth = visibleStep.width;
+				}
+			}
+			targetHeight = originalHeight - ($toHide.outerHeight(true) - $toShow.outerHeight(true));
+			targetWidth = originalWidth - ($toHide.outerWidth(true) - $toShow.outerWidth(true));
+			//save current animation data
+			var data = {
+				start: {
+					height: originalHeight,
+					width: originalWidth,
+					el: toHide
+				},
+				target: {
+					height: targetHeight,
+					width: targetWidth,
+					el: $toShow
+				}
+			};
+			$this.data('animateContentSwich.animating', data);
+			var hide = function() {
+				$toHide.animate({opacity: 0}, speed, function(){
+					$toHide.hide();
+					if ( $.isFunction( callbacks.beforeShow ) ) {
+						callbacks.beforeShow(show);
+					} else {
+						show();
+					}
+				});
+			}
+
+			var show = function () {
+
+				// Using Deferred objects for a `group` callback
+
+				var toShowDfd = $.Deferred();
+				$toShow.css('opacity',0);
+				$toShow.show();
+				$toShow.animate({opacity: 1}, speed, toShowDfd.resolve);
+
+				var heightDfd = $.Deferred();
+                var animationOptions = {
+                    "speed": speed,
+                    complete: heightDfd.resolve
+                }
+                if ( $.isFunction( callbacks.step ) ) {
+                    animationOptions.step = callbacks.step;
+                }
+				$this.animate( {
+                    height: targetHeight,
+                    width: targetWidth
+                }, animationOptions);
+
+				$.when(toShowDfd, heightDfd).then(function () // animate to final dimensions
+				{
+					$this.removeData('animateContentSwich.previous');
+					if ( $.isFunction( callbacks.final ) ) {
+						callbacks.final();
+					}
+				});
+			}
+			hide();
+		} );
+	};
+
+
+})( jQuery );

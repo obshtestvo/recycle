c5d2d1117716588b910995e404fdaae0b5c8479e
Anton Stoychev <antitoxic@gmail.com>
2013-10-31 00:44:44 +0200

    Refactoring. Moved Streetviw out of PopUp logic. Moved all non popup or non-streetview logic in location wizard. Made separate classes for each location wizard step

--- static/js/map/popupNew.js
+++ static/js/map/popupNew.js
@@ -11,9 +11,8 @@ var PopupAddNew;
      * @param callback
      * @constructor
      */
-    PopupAddNew = function(options, callback) {
+    PopupAddNew = function(options) {
         var o = $.extend({
-            addressInputSelector: ".floater div.add-new .new-address",
             map: undefined,
             location: undefined,
             content: "Heyyy!",
@@ -41,137 +40,19 @@ var PopupAddNew;
             _self.infowindow = _self._createInfoWindow({"content": o.content})
             _self.marker.setVisible(true);
             _self.infowindow.open(_self.map, _self.marker);
-            _self.map.setOptions({draggableCursor: 'pointer'});
-            _self.$elements = {}
-            var createMapClickListener = function() {
-                return gMap.event.addListener(_self.map, 'click', function (event) {
-                    //this is how we access the streetview params _self.streetview.pov
-                    _self.markerPositionChangedRecently = true;
-                    if (_self.markerPositionChangedTimeout) {
-                        clearTimeout(_self.markerPositionChangedTimeout);
-                    }
-                    _self._animateMarker(event.latLng, function () {
-                        _self.checkStreetView(_self.marker.getPosition());
-                        gMap.event.trigger(_self.marker, 'position_changed_custom');
-                        _self.timeoutMarkerPositionChange();
-                    });
-                })
-            }
             _self.events = {
                 // The infowindow DOM is ready
                 infoWindowReady: gMap.event.addListener(_self.infowindow, 'domready', function() {
-                    var $content = $('#add-new');
-                    _self.$elements.close = $content.find('a.close')
-                    _self.$elements.streetview = $content.find('.streetview')
-                    _self.$elements.step1 = $content.find('#step1');
-                    _self.$elements.streetviewCancelTrigger = _self.$elements.step1.find('a.cancel-streetview');
-                    _self.$elements.streetviewReactivateTrigger = _self.$elements.step1.find('a.reactivate');
-                    _self.$elements.step1DoneTrigger = _self.$elements.step1.find('a.accept');
-                    _self.$elements.streetviewCancelled = $content.find('.cancelled-streetview');
-
+                    _self.$infoWindowContainer = $('#add-new');
+                    _self.trigger('ready');
+                    var $close = _self.$infoWindowContainer.find('a.close')
 
                     // Custom close link on the infowindow
-                    _self.$elements.close.click(function(e){
+                    $close.click(function(e){
                         e.preventDefault();
                         gMap.event.trigger(_self.infowindow, "closeclick");
                         _self.infowindow.close();
                     })
-
-                    // Cancel streetview
-                    _self.$elements.streetviewCancelTrigger.click(function(e) {
-                        e.preventDefault();
-                        _self._cancelStreetView()
-                        $(this).addClass('hide')
-                        _self.$elements.streetviewReactivateTrigger.removeClass('hide')
-                    })
-
-                    // Reactivate streetview
-                    _self.$elements.streetviewReactivateTrigger.click(function(e) {
-                        e.preventDefault();
-                        _self.checkStreetView(_self.marker.getPosition());
-                        $(this).addClass('hide')
-                        _self.$elements.streetviewCancelTrigger.removeClass('hide')
-                    })
-                    _self.$elements.streetviewHolding = $content.find('.missing-streetview')
-
-                     // Create streetview and sync its location with the marker
-                    _self.streetview = _self._createStreetView(_self.$elements.streetview.get(0));
-                    _self.streetview.bindTo("position", _self.marker);
-                    _self.checkStreetView(_self.marker.getPosition());
-
-                    // Step 1 is complete
-                    _self.$elements.step1DoneTrigger.click(function(e){
-                        e.preventDefault();
-                        var src = 'http://placehold.it/300x100';
-                        var $step2img = _self.$elements.step2.find('.street img');
-                        if (_self.$elements.streetview.is(':visible')) {
-                            src = _self._getPhotoUrl($step2img.width(), $step2img.height())
-                        }
-                        $step2img.attr('src',src)
-                        _self.infowindow.switchContent($content, _self.$elements.step1, _self.$elements.step2, 100)
-                        _self.map.setOptions({draggableCursor: 'url(https://maps.gstatic.com/mapfiles/openhand_8_8.cur),default'});
-                        _self.marker.setOptions({cursor: 'url(https://maps.gstatic.com/mapfiles/openhand_8_8.cur),default'});
-                        _self.marker.setDraggable(false);
-                        gMap.event.removeListener(_self.events.mapClick);
-                    })
-
-                    // Step 2
-                    _self.$elements.step2 = $content.find('#step2');
-
-                    // Go back to step1
-                    _self.$elements.step2backTrigger = _self.$elements.step2.find('a.back');
-                    _self.$elements.step2backTrigger.click(function(e) {
-                        e.preventDefault();
-                        _self.infowindow.switchContent($content, _self.$elements.step2, _self.$elements.step1, 100)
-                        _self.marker.setDraggable(true);
-                        _self.events.markerDrag = createMapClickListener();
-                        _self.map.setOptions({draggableCursor: 'pointer'});
-                        _self.marker.setOptions({cursor: 'pointer'});
-                    });
-                    _self.$elements.step2DoneTrigger = _self.$elements.step2.find('a.accept');
-                    _self.$elements.step2DoneTrigger.click(function(e){
-                        e.preventDefault();
-                        _self.$elements.step2.block({
-                            message: null,
-                            overlayCSS: {
-                                backgroundColor:'rgba(255, 255, 255, 0.6)',
-                                opacity:1
-                            }
-                        });
-                        var $veil = _self.$elements.step2.find('.blockOverlay');
-                        new Spinner({
-                            top: 'auto',
-                            left: 'auto',
-                            lines: 15, // The number of lines to draw
-                            length: 0, // The length of each line
-                            width: 5, // The line thickness
-                            radius: 4, // The radius of the inner circle
-                            corners: 1, // Corner roundness (0..1)
-                            color: '#65E034', // #rgb or #rrggbb
-                            speed: 1, // Rounds per second
-                            trail: 31, // Afterglow percentage
-                            shadow: false, // Whether to render a shadow
-                            hwaccel: true // Whether to use hardware acceleration
-                        }).spin($veil.get(0));
-
-                        // simulate ajax
-                        location_data = app.locationWizard.getMapInput();
-                        var data = $content.find('input,select,textarea').serialize() + "&lat=" + location_data['loc']['lat'] +"&lng=" +location_data['loc']['lng']+ "&address=" +location_data['address']['simple']['street'] + "&streetview_params=" + JSON.stringify(location_data['streetview']);
-
-                        $.ajax({
-                          url: '/spots/',
-                          type: 'PUT',
-                          data: data,
-                          success: function() {
-                            _self.$elements.step2.unblock();
-                            _self.infowindow.switchContent($content, _self.$elements.step2, _self.$elements.step3, 100);
-                          }
-                        });
-                    })
-
-                    _self.$elements.step3 = $content.find('#step3');
-
-
                 }),
 
                 // When the infowindow is closed
@@ -185,37 +66,29 @@ var PopupAddNew;
                     _self.markerPositionChangedRecently = true;
                 }),
 
-                // When the marker is dragged
-                markerDrag: gMap.event.addListener(_self.marker, "dragend", function () {
-                    _self.markerPositionChangedRecently = false;
-                    _self.checkStreetView(_self.marker.getPosition());
-                }),
-
                 // When the marker is dragged or address is changed
                 markerPositionChange: gMap.event.addListener(_self.marker, "position_changed", function () {
                     if (!_self.markerPositionChangedRecently) {
-                        gMap.event.trigger(_self.marker, 'position_changed_custom');
+                        _self.trigger('move', [_self.marker.getPosition()]);
                         _self.timeoutMarkerPositionChange();
                     }
-                }),
-
-                // When the map is clicked
-                mapClick: createMapClickListener()
+                })
             }
-            callback(_self);
+            _self.enableMoving()
         })
     }
-    PopupAddNew.prototype = {
+
+    PopupAddNew.prototype = $.extend({}, EventEmitter(), {
         map: null,
         addressInfo: null,
         marker: null,
         markerPositionChangedRecently: false,
         markerPositionChangedTimeout: false,
+        $infoWindowContainer: null,
         infowindow: null,
-        streetview: null,
         geo: null,
         events: null,
-        $elements: null,
+
         /**
          * Add a marker
          * @param location LatLng location
@@ -240,6 +113,7 @@ var PopupAddNew;
             }, options)
             return new gMap.Marker(options);
         },
+
         /**
          * Creates an infowindow
          *
@@ -263,39 +137,9 @@ var PopupAddNew;
             }, options)
             $content.remove();
             var infoWindow = new InfoBoxAnimated(options);
-//            var options = $.extend({}, options)
-//            var infoWindow = new gMap.InfoWindow(options);
             return infoWindow;
         },
-        /**
-         * Cancels streetview picker
-         * @private
-         */
-        _cancelStreetView: function() {
-            var _self = this;
-            if (_self.$elements.streetviewCancelled.is(':visible')) return;
-            _self.$elements.streetviewHolding.hide();
-            _self.$elements.streetview.hide();
-            _self.streetview.setVisible(false);
-            _self.$elements.streetviewCancelled.show();
-        },
-        /**
-         * Creates a streetview obj
-         *
-         * @param el DOM el
-         * @param options
-         * @returns {gMap.StreetViewPanorama}
-         * @private
-         */
-        _createStreetView: function(el, options) {
-            var options = $.extend({
-                navigationControl: false,
-                enableCloseButton: false,
-                addressControl: false,
-                linksControl: false
-            }, options)
-            return new gMap.StreetViewPanorama(el, options);
-        },
+
         /**
          * Move marker smoothly
          * @param location
@@ -309,40 +153,40 @@ var PopupAddNew;
                 complete: callback
             });
         },
+
         /**
-         * Check if streetview is out of range
-         * @param location LatLng
+         * Enable moving the marker
          */
-        checkStreetView: function(location){
+        enableMoving: function() {
             var _self = this;
-            _self.geo.panorama.getPanoramaByLocation(location, 50, function(result, status) {
-                _self.$elements.streetviewCancelled.hide()
-                if (status == gMap.StreetViewStatus.OK) {
-                    _self.$elements.streetview.show();
-                    _self.streetview.setVisible(true);
-                    _self.$elements.streetviewHolding.hide();
-                } else {
-                    _self.$elements.streetview.hide();
-                    _self.streetview.setVisible(false);
-                    _self.$elements.streetviewHolding.show()
+            var listener = gMap.event.addListener(_self.map, 'click', function (event) {
+                _self.markerPositionChangedRecently = true;
+                if (_self.markerPositionChangedTimeout) {
+                    clearTimeout(_self.markerPositionChangedTimeout);
                 }
-            });
+                _self._animateMarker(event.latLng, function () {
+                    _self.trigger('move', [_self.marker.getPosition()]);
+                    _self.timeoutMarkerPositionChange();
+                });
+            })
+            _self.marker.setDraggable(true);
+            _self.events.mapClick = listener;
+            _self.map.setOptions({draggableCursor: 'pointer'});
+            _self.marker.setOptions({cursor: 'pointer'});
+
+            return listener;
         },
 
         /**
-         * Get url to static image representing Google Street View
-         * @param height
-         * @param width
-         * @returns {string} Url to image
-         * @private
+         * Disable moving the marker
          */
-        _getPhotoUrl: function(width, height) {
-            var pov = this.streetview.getPov()
-            var loc = this.marker.getPosition()
-            var values = [120, 90, 53.5, 28.3, 14.3, 10];
-            var fov = values[Math.round(pov.zoom)];
-            return 'http://maps.googleapis.com/maps/api/streetview?size='+width+'x'+height+'&location='+loc.toUrlValue()+'&fov='+fov+'&heading='+pov.heading+'&pitch='+pov.pitch+'&sensor=false';
+        disableMoving: function() {
+            this.map.setOptions({draggableCursor: 'url(https://maps.gstatic.com/mapfiles/openhand_8_8.cur),default'});
+            this.marker.setOptions({cursor: 'url(https://maps.gstatic.com/mapfiles/openhand_8_8.cur),default'});
+            this.marker.setDraggable(false);
+            gMap.event.removeListener(this.events.mapClick);
         },
+
         /**
          * Clean after the popup is not in use
          */
@@ -356,16 +200,16 @@ var PopupAddNew;
             gMap.event.removeListener(_self.events.infoClose);
             if (_self.infowindow.getMap() instanceof gMap.Map)
                 _self.infowindow.close()
-            _self.streetview.unbind("position");;
             _self.marker.setMap(null);
         },
+
         /**
-         * Delays marker position change by ceratin milliseconds.
+         * Delays marker position change by certain milliseconds.
          */
         timeoutMarkerPositionChange: function() {
             var _self = this;
             _self.markerPositionChangedRecently = true;
             _self.markerPositionChangedTimeout = setTimeout(function() {_self.markerPositionChangedRecently = false;}, 200)
         }
-    }
+    });
 })(google.maps)
---
+++ static/js/map/streetViewPicker.js
@@ -0,0 +1,124 @@
+/**
+ * Export
+ */
+var StreetViewPicker;
+
+(function(gMap) {
+
+    StreetViewPicker = function ($el, geo, options) {
+        var _self = this;
+
+        options = $.extend({
+            closedPlaceholderSelector: undefined,
+            unavailablePlaceholderSelector: undefined,
+            closeSelector: undefined,
+            triggerSelector: undefined,
+            availabilityRange: 50 // meters
+        }, options);
+
+        _self.$container = $el;
+        _self.$closedPlaceholder = $(options.closedPlaceholderSelector);
+        _self.$unavailablePlaceholder = $(options.unavailablePlaceholderSelector);
+        _self.$close = $(options.closeSelector);
+        _self.$trigger = $(options.triggerSelector);
+        _self.availabilityRange = options.availabilityRange;
+        _self.geo = geo;
+
+        _self.streetview = new gMap.StreetViewPanorama(_self.$container.get(0), {
+            navigationControl: false,
+            enableCloseButton: false,
+            addressControl: false,
+            linksControl: false
+        })
+
+        // Cancel streetview
+        _self.$close.click(function(e) {
+            e.preventDefault();
+            _self.cancel()
+            _self.$close.addClass('hide')
+            _self.$trigger.removeClass('hide')
+        })
+
+        // Reactivate streetview
+        _self.$trigger.click(function(e) {
+            e.preventDefault();
+            _self.handleAvailability(_self.marker.getPosition());
+            _self.$trigger.addClass('hide')
+            _self.$close.removeClass('hide')
+        })
+
+    }
+
+    StreetViewPicker.prototype = {
+        $container: null,
+        $closedPlaceholder: null,
+        $unavailablePlaceholder: null,
+        $trigger: null,
+        $close: null,
+        streetview: null,
+        availabilityRange: null,
+
+        /**
+         * Cancels streetview picker
+         * @private
+         */
+        cancel: function() {
+            var _self = this;
+            if (_self.$closedPlaceholder.is(':visible')) return;
+            _self.$unavailablePlaceholder.hide();
+            _self.$container.hide();
+            _self.streetview.setVisible(false);
+            _self.$closedPlaceholder.show();
+        },
+        /**
+         * Check if streetview is out of range
+         * @param location LatLng
+         */
+        handleAvailability: function(location){
+            var _self = this;
+            _self.geo.panorama.getPanoramaByLocation(location, _self.availabilityRange, function(result, status) {
+                _self.$closedPlaceholder.hide()
+                if (status == gMap.StreetViewStatus.OK) {
+                    _self.$container.show();
+                    _self.streetview.setVisible(true);
+                    _self.$unavailablePlaceholder.hide();
+                } else {
+                    _self.$container.hide();
+                    _self.streetview.setVisible(false);
+                    _self.$unavailablePlaceholder.show()
+                }
+            });
+        },
+
+        /**
+         * Get url to static image representing Google Street View
+         * @param height
+         * @param width
+         * @returns {string} Url to image
+         * @private
+         */
+        getSnapshotUrl: function(width, height) {
+            var pov = this.streetview.getPov()
+            var loc = this.streetview.getPosition()
+            var values = [120, 90, 53.5, 28.3, 14.3, 10];
+            var fov = values[Math.round(pov.zoom)];
+            return 'http://maps.googleapis.com/maps/api/streetview?size='+width+'x'+height+'&location='+loc.toUrlValue()+'&fov='+fov+'&heading='+pov.heading+'&pitch='+pov.pitch+'&sensor=false';
+        },
+
+        /**
+         * Bind streetview position to marker's position
+         * @param {gMap.Marker} marker
+         */
+        bindToMarker: function(marker) {
+            this.streetview.bindTo("position", marker);
+        },
+
+        /**
+         * Whether the streetview panorama is visible
+         * @returns boolean
+         */
+        isActive: function() {
+            return this.$container.is(':visible');
+        }
+    }
+})(google.maps)

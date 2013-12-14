eee0c9353d039d61666013d6ca362eb1a2c1fc83
Kristiyan Gospodinov <k.gospodinov@avalith.bg>
2013-12-14 15:18:50 +0200

    Recyclable item label on admin

--- ecomap/admin.py
+++ ecomap/admin.py
@@ -11,7 +11,10 @@ class RecycleSpotTypeAdmin(admin.ModelAdmin):
 class RecycleSpotMaterialAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
+class RecyclableItemAdmin(admin.ModelAdmin):
+    list_display = ('alias',)
+
 admin.site.register(RecycleSpot,RecycleSpotAdmin)
 admin.site.register(RecycleSpotType, RecycleSpotTypeAdmin)
 admin.site.register(RecycleSpotMaterial, RecycleSpotMaterialAdmin)
-admin.site.register(RecyclableItem)
+admin.site.register(RecyclableItem,RecyclableItemAdmin)

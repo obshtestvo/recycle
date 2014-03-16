21086c77ec526a25ff8261abb7312c91faeb8f94
antitoxic <antitoxic@gmail.com>
2014-03-16 21:22:30 +0200

    Jsonify is not safe.

--- restful/templatetags/restful.py
+++ restful/templatetags/restful.py
@@ -3,7 +3,6 @@ import json
 from django.core.serializers import serialize
 from django.db.models.query import QuerySet
 from django import template
-from django.utils import safestring
 
 register = template.Library()
 
@@ -11,4 +10,4 @@ register = template.Library()
 def jsonify(object):
     if isinstance(object, QuerySet):
         return serialize('json', object)
-    return safestring.mark_safe(json.dumps(object))
+    return json.dumps(object)

c19e81ce8f377240936581053c17c1a19f12770f
antitoxic <antitoxic@gmail.com>
2014-03-16 19:54:08 +0200

    Moved allowed hosts to local settings

--- settings.py
+++ settings.py
@@ -10,9 +10,6 @@ MANAGERS = ADMINS
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 SESSION_ENGINE = "django.contrib.sessions.backends.file"
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
